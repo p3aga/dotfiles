a53e326fe1dc825548559494a80b7b57739c7781
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-03-30 15:35:02 -0300

    Add Homebrew environment setup to Zsh configuration

--- dot_zshrc.d/00-env.zsh
+++ dot_zshrc.d/00-env.zsh
@@ -6,3 +6,8 @@ export VAGRANT_DEFAULT_PROVIDER=libvirt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
+# Homebrew environment (Linuxbrew default path).
+if [[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
