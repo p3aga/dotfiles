be2c739ee57f0d8cbae714404d35e35747c35508
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-04-01 11:39:45 -0300

    Set vagrant default provider only if vagrant executable exists

--- dot_zshrc.d/00-env.zsh
+++ dot_zshrc.d/00-env.zsh
@@ -1,6 +1,9 @@
 # Environment variables and shell startup performance.
 export EDITOR=nvim
+
+{{- if findExecutable "vagrant" "" }}
 export VAGRANT_DEFAULT_PROVIDER=libvirt
+{{- end }}
 
 # Powerlevel10k instant prompt should stay near the top of the startup.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
