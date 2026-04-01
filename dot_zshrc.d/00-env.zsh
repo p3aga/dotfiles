1c8c90046395d3059d3d15b173aa7abc4d5fc4cc
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-04-01 16:17:12 -0300

    Refactor vagrant provider check to use command -v for improved readability

--- dot_zshrc.d/00-env.zsh
+++ dot_zshrc.d/00-env.zsh
@@ -1,9 +1,9 @@
 # Environment variables and shell startup performance.
 export EDITOR=nvim
 
-{{- if findExecutable "vagrant" "" }}
-export VAGRANT_DEFAULT_PROVIDER=libvirt
-{{- end }}
+if command -v vagrant &> /dev/null; then
+  export VAGRANT_DEFAULT_PROVIDER=libvirt
+fi
 
 # Powerlevel10k instant prompt should stay near the top of the startup.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
