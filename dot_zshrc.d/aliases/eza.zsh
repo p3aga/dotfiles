b399715d1c74f02241d60ff857d5242d52cb5f77
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-05-19 00:54:54 -0300

    fix: eza icons completion

--- dot_zshrc.d/aliases/eza.zsh
+++ dot_zshrc.d/aliases/eza.zsh
@@ -1,5 +1,5 @@
 # eza aliases.
-alias el='eza --icons'
-alias ell='eza -l --icons'
-alias ella='eza -la --icons'
+alias el='eza --icons=always'
+alias ell='eza -l --icons=always'
+alias ella='eza -la --icons=always'
 alias tree='eza --tree --git-ignore'
