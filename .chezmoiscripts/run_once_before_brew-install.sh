b8cd048931d57de7e7c178df24c339492701a868
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-06-29 20:00:34 -0300

    feat: add brew trust taps in chezmoiscripts

--- .chezmoiscripts/run_once_before_brew-install.sh
+++ .chezmoiscripts/run_once_before_brew-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-{{ if not (findExecutable "brew" "") }}
+if command -v "brew" > /dev/null 2>&1; then
   echo "Installing Homebrew"
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-{{ end }}
+fi
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
